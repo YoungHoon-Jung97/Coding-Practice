a9a3ca49224e03d696ae2cf507f5a85f314e95c8
YongHoon-Jung97 <sunmoon0906@naver.com>
2025-05-24 23:12:21 +0900

    feat: Solve programmers LV.1 아픈 동물 찾기

--- programmers/Level1/아픈 동물 찾기/solution_131535.sql
+++
@@ -1,7 +0,0 @@
-
---¹®Á¦ :https://school.programmers.co.kr/learn/courses/30/lessons/131535
-SELECT COUNT(*) AS USERS
-FROM USER_INFO 
-WHERE AGE IS NOT NULL AND JOINED >='2021-01-01'
-    AND JOINED <='2021-12-31' AND 
-    AGE BETWEEN 20 AND 29;
